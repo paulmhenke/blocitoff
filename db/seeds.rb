2643f7d2643b611f8e3c645a8b90df57eabfb927
paulhenke <paulmhenke@gmail.com>
2015-05-16 18:08:41 +0000

    minimal Seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,17 @@
+require 'faker'
+
+member = User.create!(name: "Member User", email: "member@example.com", password: "helloworld", id: 100)
+member.skip_confirmation!
+member.save!   
+    
+10.times do
+Item.create!(name: Faker::Lorem.sentence, user_id: 100)
+end
+
+puts "Seed Finished"
+puts "#{Item.count} items created"
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
