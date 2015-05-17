5f6e0337001989a6f290cca18b6983c72f19b641
paulhenke <paulmhenke@gmail.com>
2015-05-17 22:45:38 +0000

    fixed item params issue in ItemsController

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,11 @@ class ItemsController < ApplicationController
  
   def create
     @user = current_user
-    @item = @user.items.build(params[:name])
+    
+    @item = @user.items.build(params.require(:item).permit(:name))
+    @item.user = @user
+    @new_item = Item.new
+  
     
     if @item.save
       redirect_to @user, notice: "Success. Now do it."
